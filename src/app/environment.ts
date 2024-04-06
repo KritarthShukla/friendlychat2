5f221b07fafc4523b2dda268c017a0360f246eea
KritarthShukla <kritarthshukla92@gmail.com>
2024-04-06 07:20:57 +0530

    done

---
+++ src/app/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDpVnEDqmCzg8TJgrOwkNG1MkWkbydL1PA",
+        authDomain: "friendlychat-adaa8.firebaseapp.com",
+        projectId: "friendlychat-adaa8",
+        storageBucket: "friendlychat-adaa8.appspot.com",
+        messagingSenderId: "350805575442",
+        appId: "1:350805575442:web:e58e678424b107e14b95cb",
+        measurementId: "G-Z2M07MZ64K"
+    }
+};
